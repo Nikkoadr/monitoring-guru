18a3220d8dc42b60c5c1fc42545ee8e0240cc17e
Nikko Adrian <nikkoadrian02@gmail.com>
2025-03-14 19:06:47 +0700

    Update presensi_pendidik.blade.php

--- resources/views/absensi/presensi_pendidik.blade.php
+++ resources/views/absensi/presensi_pendidik.blade.php
@@ -289,7 +289,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                             const distance = faceapi.euclideanDistance(descriptorBaru, descriptorTersimpan);
 
-                            if (distance < 0.5) {
+                            if (distance < 0.6) {
                                 if (checkLocation(userLatitude, userLongitude)) {
                                     kirimPresensi(url);
                                 } else {
